d2a138f1b967a6ce9193231efa77d6fb50651a73
AnbuR11 <anbarasan.ramanathan@capgemini.com>
2025-01-13 10:11:00 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,6 @@
+function skillsMember() {
+  var member = document.getElementsByClassName("member");
+  for (var i = 0; i < member.length; i++) {
+    member[i].classList.add("skills-member");
+  }
+}
